390b30c4231b5a2d7da08f8a6ccdd5c71fdcb508
cairuisi <ajayjaiswal@utexas.edu>
2024-03-07 22:04:59 -0600

    march72302

---
+++ chat_generation.py
@@ -0,0 +1,40 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="6,7"
+from typing import List, Optional
+
+import fire
+# torchrun --nproc_per_node 1 chat_generation.py  --max_seq_len 512 --max_batch_size 1
+from llama import Llama, Dialog
+
+def main(
+    ckpt_dir = "/data/ajay_data/llama2_models/llama-2-13b-chat/",
+    tokenizer_path = "/data/ajay_data/llama2_models/tokenizer.model",
+    temperature: float = 0.6,
+    top_p: float = 0.9,
+    max_seq_len: int = 512,
+    max_batch_size: int = 1,
+    max_gen_len: Optional[int] = None,
+):
+    generator = Llama.build(
+        ckpt_dir=ckpt_dir,
+        tokenizer_path=tokenizer_path,
+        max_seq_len=max_seq_len,
+        max_batch_size=max_batch_size,
+    )
+
+    dialogs: List[Dialog] = [
+        [{"role": "user", "content": "Can you plan a trip to Miami?"}]
+    ]
+
+    
+    result = generator.chat_completion(
+        dialogs,  # type: ignore
+        max_gen_len=max_gen_len,
+        temperature=temperature,
+        top_p=top_p,
+    )
+    print(f"{dialogs[0][0]['role'].capitalize()}: {dialogs[0][0]['content']}\n")
+    print(f"> {result[0]['generation']['role'].capitalize()}: {result[0]['generation']['content']}")
+
+if __name__ == "__main__":
+    fire.Fire(main)
---
+++ llama/utils.py
@@ -0,0 +1,19 @@
+import torch
+
+def calc_skip_pattern(interleave, max_seq_len):
+    skip_plot = []
+    for i in range(0, max_seq_len // 10):
+        skip_plot.append(0)
+
+    j = max_seq_len // 10
+    while j < max_seq_len:
+        # skip_plot.extend(list(range(0, max_height)))
+        # skip_pattern = list([0, 1, 2, 4, 6, 8])
+        # skip_pattern = [10, 12, 14, 12, 10]
+        skip_pattern = [0, 0, 0]
+        skip_plot.extend(skip_pattern)
+        skip_plot.extend(list([0] * interleave))
+        j += (len(skip_pattern) + interleave)
+    return skip_plot
+
+    
